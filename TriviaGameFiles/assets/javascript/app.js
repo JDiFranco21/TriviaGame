32e0ebf76a1017d9c11c2d802f0914e3162607bc
Joe <jdifranco21@gmail.com>
2018-01-07 17:10:43 -0600

    first commit

---
+++ TriviaGameFiles/assets/javascript/app.js
@@ -0,0 +1,33 @@
+var questionList =
+      [
+        {
+          question: "Who Shot First?",
+          choices: ["Greedo", "Han", "Boba", "Luke"],
+          answer: 2
+        },
+
+        {
+          question: "Which is the only film  in which desert planet Tatooine doesn’t appear?",
+          choices: ["Emprie Strikes Back", "A New Hope", "Return of the Jedi", "Revenge of the Sith"],
+          answer: 1
+        }
+
+        {
+          question: "Who kills Jabba The Hutt?",
+          choices: ["Han", "Luke", "Leia", "Chewbacca"],
+          answer: 3
+        }
+
+        {
+          question: "Han Solo and the Millennium Falcon can complete the Kessel Run in how many parsecs?",
+          choices: ["12", "Less than 12", "11", "parsecs are not a unit of distance not time"],
+          answer: 2
+        }
+
+        {
+          question: "What does Luke’s uncle Owen do for a living?",
+          choices: ["Part Salvager", "Speeder Mechanic", "Cave Hermit", "Moisture Farmer"],
+          answer: 4
+        }
+
+      ];
